21cd2b1cae7cb99434db4f23a405147441c12282
Marco Antonio Gusmao Carvalho <marcocarvalho.web@gmail.com>
2019-09-01 20:10:34 -0300

    Tratamento de Requisicao

--- src/Application.php
+++ src/Application.php
@@ -8,6 +8,10 @@ class Application {
     public function setRequestHandlerNamespace(string $namespace) : void {
         $this->requestHandlerNamespace = $namespace;
     }
+
+    public function getRequestHandlerNamespace() : string {
+        return $this->requestHandlerNamespace;
+    }
     
     public function setDefaultUrl(string $url) : void {
         $this->defaultUrl = $url;
@@ -17,11 +21,26 @@ class Application {
         return $this->defaultUrl;
     }
 
+    private function parseUri(string $uri, array $info = []) : array {
+        $className = $this->getRequestHandlerNamespace();
+        foreach(explode('/', $uri) as $segment)
+            $className .= '\\' . ucfirst($segment);
+        $className .= 'Handler';
+        if(class_exists($className))
+            return ['class' => $className, 'parameters' => $info];
+        else {
+            $info[] = substr($uri, strrpos($uri, '/') + 1);
+            $uri = substr($uri, 0, strrpos($uri, '/'));
+            return $this->parseUri($uri, $info);
+        }
+    }
+
     private function parseRequest() {
         $requestInfo = [
             'method' => strtolower($_SERVER['REQUEST_METHOD']),
             'uri' => (trim($_SERVER['REQUEST_URI'], '/') == '') ? $this->getDefaultUrl() : trim($_SERVER['REQUEST_URI'], '/') == ''
         ];
+        $requestInfo['class'] = parseUri($requestInfo['uri']);
         return $requestInfo;
     }
 
