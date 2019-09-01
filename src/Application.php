45264d913fd0d3a5604085bf14fbd4d586ea7467
Marco Antonio Gusmao Carvalho <marcocarvalho.web@gmail.com>
2019-09-01 19:37:53 -0300

    Tratamento de Requisicao

--- src/Application.php
+++ src/Application.php
@@ -2,7 +2,22 @@
 namespace PapayaWithSugar;
 
 class Application {
+    private $requestHandlerNamespace = null;
+    private $defaultUrl = null;
+
+    public function getRequestHandlerNamespace(string $namespace) : void {
+        $this->requestHandlerNamespace = $namespace;
+    }
+    
+    public function setDefaultUrl(string $url) : void {
+        $this->defaultUrl = $url;
+    }
+
+    private function parseRequest() {
+        return $_SERVER['REQUEST_URI'];
+    }
+
     public function run() {
-        return "Working";
+        return $this->parseRequest();
     }
 }
