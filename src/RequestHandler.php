2f9921b49748801ae58e27b303a55e7eab33fc2c
Marco Antonio Gusmao Carvalho <marcocarvalho.web@gmail.com>
2019-09-02 11:20:49 -0300

    Views

--- src/RequestHandler.php
+++ src/RequestHandler.php
@@ -3,7 +3,7 @@ namespace PapayaWithSugar;
 
 class RequestHandler {
     public function view(array $parameters = []) {
-        $viewPath = '/public/views/';
+        $viewPath = '/public/views/' . get_class($this);
         return $viewPath;
     }
 }
