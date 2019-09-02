c213fd4c62aa0a510710f56ae46359c77cc489bf
Marco Antonio Gusmao Carvalho <marcocarvalho.web@gmail.com>
2019-09-02 11:12:58 -0300

    Views

--- src/Application.php
+++ src/Application.php
@@ -58,9 +58,10 @@ class Application {
 
     public function run() {
         $request = $this->parseRequest();
-        if(!is_null($request['class']))
-            return call_user_func_array([$request['class'], $request['method']], $request['parameters']);
-        else {
+        if(!is_null($request['class'])) {
+            $handler = new $request['class']();
+            return call_user_func_array([$handler, $request['method']], $request['parameters']);
+        } else {
             http_response_code(404);
             die();
         }
