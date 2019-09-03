fcdd952e9d2ccfe4ce650294e1670d8c8bee678b
Marco Antonio Gusmao Carvalho <marcocarvalho.web@gmail.com>
2019-09-03 08:07:43 -0300

    Views

--- src/RequestHandler.php
+++ src/RequestHandler.php
@@ -10,7 +10,7 @@ class RequestHandler {
 
     public function view(array $parameters = []) {
         $documentRoot = $_SERVER['DOCUMENT_ROOT'];
-        $viewsRoot = $documentRoot . (substr($documentRoot, strrpos($documentRoot, DIRECTORY_SEPARATOR) + 1) == 'public') ? '/views' : '/public/views';
+        $viewsRoot = $documentRoot . ((substr($documentRoot, strrpos($documentRoot, DIRECTORY_SEPARATOR) + 1) == 'public') ? '/views' : '/public/views');
         $viewFile = $viewsRoot . strtolower(str_replace([$this->application->getRequestHandlerNamespace(), '\\', 'Handler'], ['', DIRECTORY_SEPARATOR, ''], get_class($this))) . '.php';
         // $viewPath = $_SERVER['DOCUMENT_ROOT'];
         // $viewPath .= (substr($viewPath, strrpos($viewPath, DIRECTORY_SEPARATOR) + 1) == 'public') ? '/views' : '/public/views';
