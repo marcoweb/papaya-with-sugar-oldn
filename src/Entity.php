281d3adb4977c7620d3ac4937972c94612889132
Marco Antonio Gusmao Carvalho <marcocarvalho.web@gmail.com>
2019-09-03 19:42:39 -0300

    Entities

--- src/Entity.php
+++ src/Entity.php
@@ -2,6 +2,18 @@
 namespace PapayaWithSugar;
 
 class Entity {
+    public function __get($name) {
+        $methodName = 'get'.ucfirst($name);
+        if(method_exists($this, $methodName))
+            return $this->$methodName();
+    }
+
+    public function __set($name, $value) {
+        $methodName = 'set'.ucfirst($name);
+        if(method_exists($this, $methodName))
+            return $this->$methodName($value);
+    }
+
     public function getConfig() {
         return [
             'table' => strtolower(substr(get_class($this), strrpos(get_class($this), '\\') + 1))
